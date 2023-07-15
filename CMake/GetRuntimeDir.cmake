e8417c820ce4896155b285d0fd30effeefac92c5
Stephen Lane-Walsh <sdl.slane@gmail.com>
2023-07-14 21:01:29 -0400

    Initial Boilerplate

---
+++ CMake/GetRuntimeDir.cmake
@@ -0,0 +1,24 @@
+# GetRuntimeDir.cmake
+#
+# Finds the runtime output directory for a given ${_target} and stores it in ${_out_var}
+#
+# This will search IMPORTED_LOCATION and IMPORTED_LOCATION_${CMAKE_BUILD_TYPE} in that order.
+#
+# This directory will need to be added to the PATH/LD_LIBRARY_PATH in order for executables
+# that link against this target to run.
+#
+
+macro(get_runtime_dir _target _out_var)
+    if(TARGET ${_target})
+        get_target_property(_location ${_target} IMPORTED_LOCATION)
+        get_filename_component(${_out_var} ${_location} DIRECTORY)
+
+        if (${_out_var} STREQUAL "" AND CMAKE_BUILD_TYPE)
+            string(TOUPPER ${CMAKE_BUILD_TYPE} _type)
+            get_target_property(_location ${_target} IMPORTED_LOCATION_${_type})
+            get_filename_component(${_out_var} ${_location} DIRECTORY)
+        endif()
+    else()
+        message(warning "get_runtime_dir() Unable to find target '${_target}'")
+    endif()
+endmacro()
---
+++ Scripts/add-launch-target.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import argparse
+import platform
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--name',
+    required=True,
+    help='Name of the config target to generate.'
+)
+
+parser.add_argument(
+    '--executable',
+    required=True,
+    help='Path to executable to run.'
+)
+
+parser.add_argument(
+    '--runtimePathList',
+    required=True,
+    help='Semicolon-delimted list of paths to search for dll/so files, will be used in PATH or LD_LIBRARY_PATH.'
+)
+
+parser.add_argument(
+    '--binaryDir',
+    required=True,
+    help='CMAKE_BINARY_DIR, will be used with executable.'
+)
+
+parser.add_argument(
+    '--workingDir',
+    required=True,
+    help='Directory to run the executable from.'
+)
+
+parser.add_argument(
+    'arguments',
+    nargs='*',
+    help='Additional arguments to pass to the executable.'
+)
+
+args = parser.parse_args()
+
+name = args.name
+executable = args.executable
+arguments = args.arguments
+runtimePathList = args.runtimePathList
+binaryDir = args.binaryDir
+workingDir = args.workingDir
+
+def add_or_update_config(data, configurations):
+    found = False
+    for i in range(0, len(configurations)):
+        if configurations[i]['name'] == data['name']:
+            configurations[i].update(data)
+            found = True
+            break
+    
+    if not found:
+        configurations.append(data)
+
+###
+### Visual Studio Code
+###
+
+if not os.path.isdir('.vscode'):
+    os.mkdir('.vscode')
+
+filename = '.vscode/launch.json'
+
+config = {}
+try:
+    file = open(filename, 'r')
+    config = json.load(file)
+except:
+    pass
+
+if 'version' not in config:
+    config['version'] = '0.2.0'
+
+if 'configurations' not in config:
+    config['configurations'] = []
+
+data = {
+    'name': name,
+    'type': 'cppdbg',
+    'request': 'launch',
+    'program': executable,
+    'args': arguments,
+    'cwd': workingDir,
+    'environment': []
+}
+
+if platform.system() == 'Windows':
+    data['type'] = 'cppvsdbg'
+    data['environment'].append({
+        'name': 'PATH',
+        'value': '${env:PATH};' + runtimePathList
+    })
+    data['console'] = 'integratedTerminal'
+else:
+    data['environment'].append({
+        'name': 'LD_LIBRARY_PATH',
+        'value': runtimePathList
+    })
+    data['externalConsole'] = False
+
+if platform.system() == 'Darwin':
+    data['MIMode'] = 'lldb'
+
+add_or_update_config(data, config['configurations'])
+
+file = open(filename, 'w')
+json.dump(config, file, indent=4)
